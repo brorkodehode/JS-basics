56cbe0222d1751d598a376c59ca1c6a6a00cd320
brorkodehode <brorkodehode@gmail.com>
2023-09-04 13:51:05 +0200

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-let mm = "Hello world"
-console.log (mm)
+let ikeaArray = [
+    "El1",
+    "El2",
+    "El3",
+    "El4",
+    "El5",
+    "El6",
+    "El7",
+    "El8",
+    "El9",
+    "EL10",
+]
+console.log(ikeaArray);
+
+ikeaArray.pop();
+
+console.log(ikeaArray);
+
+ikeaArray.push("BrødKniv");
+
+console.log(ikeaArray);
+
+ikeaArray.pop(2);
+ikeaArray.pop(6);
+
+console.log(ikeaArray);
+console.log(ikeaArray.length);
+
+
